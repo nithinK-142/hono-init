7bd1f0e6b13f6dcfbf093841603b204cad6e266d
nithinK-142 <nithin.sagar.359@gmail.com>
2024-04-22 12:47:16 +0530

    refactor: streamText route.

--- src/routes/streamText.ts
+++ src/routes/streamText.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { streamText } from "hono/streaming";
 
 export const streamTextRouter = new Hono<{ Bindings: { app: Hono } }>().get(
-  "/streamText",
+  "/",
   (c) => {
     return streamText(c, async (stream) => {
       const words = "Hello world, this is Hono streaming!".split(" ");
